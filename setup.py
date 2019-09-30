ccdecbe86fbd42d0bbb92798709b6fe9a01bdfaa
JorryZ <45359771+JorryZ@users.noreply.github.com>
2019-09-30 17:03:11 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
   url = 'https://github.com/JorryZ/motionConstrain',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/JorryZ/motionConstrain/archive/v3.1.0.tar.gz',    # I explain this later on
   keywords = ['explicit', 'motion', 'incompressibility', 'regularization'],   # Keywords that define your package best
-  install_requires=['numpy','math','medImgProc','scipy','trimesh','motionSegmentation'],
+  install_requires=['numpy','medImgProc','scipy','trimesh','motionSegmentation'],
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package    
     'Intended Audience :: Developers',      # Define that your audience are developers
