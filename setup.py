07c0be5d52157b7f0c08035976b124566f37be18
JorryZ <45359771+JorryZ@users.noreply.github.com>
2021-03-11 13:29:50 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 setup(
   name = 'motionConstrain',         # How you named your package folder (MyLib)
   packages = ['motionConstrain'],   # Chose the same as "name"
-  version = '5.0.8',      # Start with a small number and increase it with every change you make
+  version = '5.0.9',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'spatial constraint (incompressible)',   # Give a short description about your library
   author = 'Yu Zheng',                   # Type in your name
   author_email = 'jorry.zhengyu@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/JorryZ/motionConstrain',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/JorryZ/motionConstrain/archive/v5.0.8.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/JorryZ/motionConstrain/archive/v5.0.9.tar.gz',    # I explain this later on
   keywords = ['explicit', 'motion', 'incompressibility', 'regularization'],   # Keywords that define your package best
   install_requires=['numpy','medImgProc','scipy','trimesh','motionSegmentation'],
   classifiers=[
